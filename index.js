93b83041867c740e1116347d32db03c9dbf2b778
Andrew Heine <andrew.heine@students.olin.edu>
2016-10-07 13:35:19 -0700

    Classadoo autocommit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// @param[string] query
 function get(query){$(query)};
 
 function randomColor()
 {
-  
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 setInterval(
@@ -11,6 +17,6 @@ function()
   //
   // Run the same code every two seconds!
   //
-  
+  get(
   
 }, 2000);
